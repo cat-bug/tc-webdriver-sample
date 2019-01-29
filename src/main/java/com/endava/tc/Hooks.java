dcc78b92b80c7dd5583034d9ac345c4baf387b34
mburunducova <maria.burunducova@endava.com>
2019-01-29 14:26:33 +0200

    refactored

--- src/main/java/com/endava/tc/Hooks.java
+++ src/main/java/com/endava/tc/Hooks.java
@@ -2,28 +2,91 @@ package com.endava.tc;
 
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.lifecycle.TestDescription;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
+import java.util.Properties;
+
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
 public class Hooks {
 
-    @After
-    public void tearDown(Scenario scenario){
-        BrowserWebDriverContainer container = ((BrowserWebDriverContainer)ScenarioContext.getInstance().getData("container"));
-        RemoteWebDriver driver = container.getWebDriver();
-        driver.quit();
-        TestDescription testDescription = new TestDescription() {
-            public String getTestId() {
-                return scenario.getId();
-            }
 
-            public String getFilesystemFriendlyName() {
-                return scenario.getName();
-            }
-        };
-        container.afterTest(testDescription, Optional.empty());
+    @Before
+    public void setup() throws IOException {
+        InputStream input = new FileInputStream("src/main/resources/browser.properties");
+        Properties prop = new Properties();
+        prop.load(input);
+        String browser = prop.getProperty("browser");
+        Boolean incontainer = Boolean.valueOf(prop.getProperty("incontainer"));
+
+        if (incontainer){
+            BrowserWebDriverContainer container = startContainer(browser);
+            ScenarioContext.getInstance().saveData("container", container);
+            ScenarioContext.getInstance().saveData("driver", container.getWebDriver());
+        } else {
+            ScenarioContext.getInstance().saveData("driver", startLocalDriver(browser));
+        }
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        RemoteWebDriver driver = ((RemoteWebDriver) ScenarioContext.getInstance().getData("driver"));
+        driver.quit();
+        BrowserWebDriverContainer container = ((BrowserWebDriverContainer) ScenarioContext.getInstance().getData("container"));
+        if (container != null) {
+            container.getWebDriver().quit();
+
+            TestDescription testDescription = new TestDescription() {
+                public String getTestId() {
+                    return scenario.getId();
+                }
+
+                public String getFilesystemFriendlyName() {
+                    return scenario.getName();
+                }
+            };
+            container.afterTest(testDescription, Optional.empty());
+        }
+    }
+
+    private RemoteWebDriver startLocalDriver(String browser) {
+        RemoteWebDriver driver;
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+            driver = new ChromeDriver(new ChromeOptions().addArguments("--disable-notifications"));
+        } else {
+            System.setProperty("webdriver.gecko.driver", "C:/drivers/geckodriver.exe");
+            driver = new FirefoxDriver(new FirefoxOptions().addArguments("--disable-notifications"));
+        }
+        return driver;
+    }
+
+    private BrowserWebDriverContainer startContainer(String browser) {
+        BrowserWebDriverContainer container = null;
+        if (browser.equalsIgnoreCase("chrome")) {
+            container = new BrowserWebDriverContainer()
+                    .withCapabilities(new ChromeOptions().addArguments("--disable-notifications"))
+                    .withRecordingMode(RECORD_ALL, new File("target"));
+        } else {
+            container = new BrowserWebDriverContainer()
+                    .withCapabilities(new FirefoxOptions().addArguments("--disable-notifications"))
+                    .withRecordingMode(RECORD_ALL, new File("target"));
+        }
+        container.start();
+        return container;
     }
 }
--- src/main/java/com/endava/tc/Stepdefs.java
+++ src/main/java/com/endava/tc/Stepdefs.java
@@ -4,85 +4,39 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
 public class Stepdefs {
 
-    private RemoteWebDriver driver;
-
-    public Stepdefs() throws IOException {
-        InputStream input = new FileInputStream("src/main/resources/browser.properties");
-        Properties prop = new Properties();
-        prop.load(input);
-        String browser = prop.getProperty("browser");
-        Boolean incontainer = Boolean.valueOf(prop.getProperty("incontainer"));
-        BrowserWebDriverContainer container = null;
-        if (browser.equals("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-notifications");
-            if (!incontainer) {
-                System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
-                driver = new ChromeDriver(options);
-            } else {
-                container = new BrowserWebDriverContainer()
-                        .withCapabilities(options)
-                        .withRecordingMode(RECORD_ALL, new File("target"));
-                container.start();
-                driver = container.getWebDriver();
-            }
-        } else if (browser.equals("firefox")) {
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--disable-notifications");
-            if (!incontainer) {
-                System.setProperty("webdriver.gecko.driver", "C:/drivers/geckodriver.exe");
-                driver = new FirefoxDriver();
-            } else {
-                container = new BrowserWebDriverContainer()
-                        .withCapabilities(options)
-                        .withRecordingMode(RECORD_ALL, new File("target"));
-                container.start();
-                driver = container.getWebDriver();
-            }
-        }
-        ScenarioContext.getInstance().saveData("container", container);
-    }
-
-
     @Given("home page is opened")
     public void openHomePage() {
+        RemoteWebDriver driver = ((RemoteWebDriver) ScenarioContext.getInstance().getData("driver"));
         driver.get("https://www.reddit.com");
     }
 
     @When("user searches (.+)")
     public void userSearchesKeyword(String keyword) {
-        WebElement search = driver.findElement(By.xpath("//input[@type='search']"));
+        WebElement search = ((RemoteWebDriver) ScenarioContext.getInstance().getData("driver")).findElement(By.xpath("//input[@type='search']"));
         search.sendKeys(keyword);
         search.sendKeys(Keys.ENTER);
+        ScenarioContext.getInstance().saveData("keyword", keyword);
     }
 
     @Then("top community in search result is (.+)")
-    public void topCommunityInSearchResultIsCommunity(String communityName) {
-        driver.manage().timeouts().implicitlyWait(1500, TimeUnit.MILLISECONDS);
-        List<WebElement> elements = driver.findElements(By.xpath("//span[contains(text(),'Communities and users')]/following-sibling::div/div/div/a/div[1]/div/div"));
+    public void topCommunityInSearchResultIsCommunity(String communityName) throws IOException {
+        RemoteWebDriver driver = ((RemoteWebDriver) ScenarioContext.getInstance().getData("driver"));
+        driver.manage().timeouts().implicitlyWait(1800, TimeUnit.MILLISECONDS);
+        List<WebElement> elements = ((RemoteWebDriver) ScenarioContext.getInstance().getData("driver")).findElements(By.xpath("//span[contains(text(),'Communities and users')]/following-sibling::div/div/div/a/div[1]/div/div"));
         WebElement first = elements.get(0);
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("target/screenshot_" + ScenarioContext.getInstance().getData("keyword") + ".png"));
         Assert.assertEquals("Community name is correct", communityName, first.getText());
     }
 
